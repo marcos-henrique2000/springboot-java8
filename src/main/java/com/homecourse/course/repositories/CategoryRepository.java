f7ef35c0a71c025bd3640db6f03e9861750a3704
Marcos Marins <marcos-henrique2000@hotmail.com>
2020-02-05 12:48:34 -0300

    Category

---
+++ src/main/java/com/homecourse/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.homecourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.homecourse.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
